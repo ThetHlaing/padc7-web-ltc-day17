9cef983741b418ffeb287054ba222700741af888
thethlaing-mmdsgit config --global user.name thethlaing-mmds <thethlaing@mm-digital-solutions.com>
2019-03-30 18:41:35 +0630

    EOD

--- src/utilies/localStorage.js
+++ src/utilies/localStorage.js
@@ -2,6 +2,9 @@
 
     export const retrieveData = (key) => {
         const data = localStorage.getItem(key);
+        if(data == null){
+            return []
+        }
         return JSON.parse(data);
     };
 
